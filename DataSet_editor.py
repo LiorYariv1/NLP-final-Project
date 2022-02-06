158215909a7487ebc0389148e15b600a7beb29b1
LiorYariv1 <lior.ya1994@gmail.com>
2022-02-06 12:50:26 +0200

    updates

--- DataSet_editor.py
+++ DataSet_editor.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import json
+
+def combine_datasets (paths):
+    col_names = ['WikiID', 'Freebase movie ID', 'Title', 'Movie release date', 'Movie box office revenue',
+                 'Movie runtime',
+                 'Movie languages', 'Movie countries', 'genres']
+    df_genre = pd.read_csv(paths.cmu_metadata, sep='\t', header=None, names=col_names)
+    df_genre['Movie languages'] = df_genre['Movie languages'].apply(lambda x: ', '.join(json.loads(x).values()))
+    df_genre = df_genre[df_genre['Movie languages'].str.contains('English')]
+    df_genre = df_genre[['WikiID', 'Title', 'genres', 'Movie languages']]
+    df_genre['genres'] = df_genre['genres'].apply(lambda x: ', '.join(json.loads(x).values()))
+    df_plots = pd.read_csv(paths.cmu_plot_summaries,
+        sep='\t', header=None, names=['WikiID', 'Plot'])
+    joined = df_genre.set_index('WikiID').join(df_plots.set_index('WikiID'), how='inner', on='WikiID')
+    wiki_df = pd.read_csv(paths.wiki)
+
 
